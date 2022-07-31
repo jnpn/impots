6df2b221e6c37355581bed85e0db5d2ba925cf89
Johan PONIN <johan.ponin.pro@gmail.com>
2022-07-31 13:12:27 +0200

    feat(hider): removed inline template, improved html script-template
    - named slots
    - bi icons
    
    usage:
    
    ```
      <hider>yolo</hider>
      <hider></hider>
    ```
    
    closes #3

--- js/components/hider.js
+++ js/components/hider.js
@@ -7,5 +7,5 @@ const Hider = Vue.component('hider', {
       hidden: false,
     };
   },
-  template: '<div class="hider"><button type="button" class="btn btn-sm btn-primary" @click="hidden = !hidden">toggle<i class="bi-gear"></i></button><slot v-if="!hidden"></slot><div>',
+  template: '#template-hider',
 });
