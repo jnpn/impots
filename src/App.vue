cf379ec36ec55611d3020c518899ab822c51ef2a
Johan PONIN <johan.ponin.pro@gmail.com>
2022-08-01 00:28:43 +0200

    feat(vue3): remove favicons

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,6 @@ useHead({
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
     },
   ],
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
-    },
-  ],
 })
 </script>
 
