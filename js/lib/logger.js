37df519b12b5e3224ad95c2d149395eca3495184
Johan PONIN <johan.ponin.pro@gmail.com>
2022-07-31 12:28:40 +0200

    feat(logger): add ~scoped logging object and cute banner
    Closes #15

---
+++ js/lib/logger.js
@@ -0,0 +1,16 @@
+function Logger (app, options={}) {
+  return {
+    log (...args) {
+      console.log(app, ...args);
+    },
+    info (...args) {
+      console.info(app, ...args);
+    },
+    warn (...args) {
+      console.warn(app, ...args);
+    },
+    debug (...args) {
+      console.debug(app, ...args);
+    },
+  }
+}
